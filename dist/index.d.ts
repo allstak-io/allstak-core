0bc82559d201442779196c86ea649230050339a2
AllStak SDK <sdk@allstak.sa>
2026-04-22 18:34:18 +0300

    fix: commit dist for git-based npm installs

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { AllStak, AllStakConfig, Breadcrumb, DatabaseModule, DbQueryItem, HeartbeatOptions, HttpRequestItem, LogEvent, LogLevel, Span, SpanData } from 'allstak-js';
---
+++ dist/index.mjs
@@ -0,0 +1,12 @@
+// src/index.ts
+import {
+  AllStak,
+  Span,
+  DatabaseModule
+} from "allstak-js";
+export {
+  AllStak,
+  DatabaseModule,
+  Span
+};
+//# sourceMappingURL=index.mjs.map
